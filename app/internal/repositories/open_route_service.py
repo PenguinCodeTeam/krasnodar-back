24047bc55c70c900d44e93744fb83345575aeb31
vanvanich9 <57123154+vanvanich9@users.noreply.github.com>
2023-11-04 20:19:36 +0100

    PINCODE-8 add open route service repository (#7)

---
+++ app/internal/repositories/open_route_service.py
@@ -0,0 +1,34 @@
+import httpx
+
+
+OPEN_ROUTE_SERVICE_URL = "https://api.openrouteservice.org/v2/directions/driving-car"
+
+
+class OpenRouteServiceRepository:
+    def __init__(self, api_key: str):
+        self.api_key = api_key
+
+    def __params(self, start_point: str, end_point: str) -> dict:
+        return {
+            "api_key": self.api_key,
+            "start": start_point,
+            "end": end_point,
+        }
+
+    def convert_point_to_str(self, point: tuple):
+        return ','.join(map(str, point))
+
+    async def get_duration(self, start_point: tuple, end_point: tuple) -> float:
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(
+                OPEN_ROUTE_SERVICE_URL,
+                params=self.__params(
+                    start_point=self.convert_point_to_str(start_point),
+                    end_point=self.convert_point_to_str(end_point),
+                ),
+            )
+            data: dict = resp.json()
+        features: dict = data.get("features", [{}])[0]
+        properties: dict = features.get("properties", {})
+        summary: dict = properties.get("summary", {})
+        return summary.get("duration", -1)
